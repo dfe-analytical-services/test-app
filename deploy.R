1c18edf39b0579a657f5e34c40302eac585b314e
Adam Robinson <adamrobinson361@gmail.com>
2018-08-22 15:54:22 +0100

    deploy script

---
+++ deploy.R
@@ -0,0 +1,11 @@
+# Create conditional app name based on branch
+
+# Set account info
+rsconnect::setAccountInfo(
+  name="department-for-education",
+  token=Sys.getenv("SHINYAPPS_TOKEN"),
+  secret=Sys.getenv("SHINYAPPS_SECRET")
+)
+
+# Deploy
+rsconnect::deployApp(appName = "test-app")
